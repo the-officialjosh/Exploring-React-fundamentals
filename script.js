4cbe40f3a9820c087ccd46cfd5bc69c070572cbf
JOSH <onyemajoshua892@gmail.com>
2023-06-10 14:31:24 +0100

    Array Map Method

--- script.js
+++ script.js
@@ -208,8 +208,25 @@ function getTotalReviewCount(book) {
 console.log(getTotalReviewCount(book));
 
 */
+function getTotalReviewCount(book) {
+  const goodreads = book.reviews?.goodreads?.reviewsCount ?? 0;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
 
+  return goodreads + librarything;
+}
 const books = getBooks();
+books;
 
 const x = [1, 2, 3, 4, 5, 6, 7].map((el) => el * 2);
 console.log(x);
+
+const titles = books.map((book) => book.title);
+titles;
+
+const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+  reviewsCount: getTotalReviewCount(book),
+}));
+
+essentialData;
